3a3f6d64aa487ec13ef91179f82064740eeb3be7
HungMark <39577296+HungMark@users.noreply.github.com>
2018-05-30 17:41:02 +0800

    Update testpost.php

--- testpost.php
+++ testpost.php
@@ -8,4 +8,12 @@
 		<input type="submit">
 	</form>
 	</body>
+	<h2>新增使用者:</h2>
+	<form action="add.php" method="post">
+		<input type="text" name="name" value=""placeholder="名稱">
+		<input type="text" name="account" value=""placeholder="帳號">
+		<input type="password" name="password" value=""placeholder="密碼">
+		<input type="submit">
+	</form>
+	</body>
 </html>
